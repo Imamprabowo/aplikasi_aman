b912c4fa25d7d2233070619af5124af3d9532047
Frisky Riana Kesumadewi <38089317+friskyriana@users.noreply.github.com>
2019-07-25 10:37:44 +0700

    Update index.php

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -266,7 +266,7 @@
     <div class="pull-right hidden-xs">
       <b>AMAN Version</b> 1.0
     </div>
-    <strong>Copyright &copy; <?=date('Y')?></strong>
+    <strong>Copyright &copy; <?=date('Y')?> <b>Mahasiswa KP Teknik Informatika (ITERA)</b> </strong>
   </footer>
 
 
