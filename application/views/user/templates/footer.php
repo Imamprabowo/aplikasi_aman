013022da7ad56cbf9da8e6afeb8c8694407a3dae
Frisky Riana Kesumadewi <38089317+friskyriana@users.noreply.github.com>
2019-07-28 14:50:54 +0700

    Update footer.php

--- application/views/user/templates/footer.php
+++ application/views/user/templates/footer.php
@@ -1,6 +1,6 @@
   <footer class="cp-footer">
     <div class="container">
-        <h3 class="title-footer">Copyright Aman App &copy; <?=date('Y')?></h3>
+        <h3 class="title-footer">Copyright Aman App &copy; <?=date('Y')?> <b>Mahasiswa KP Teknik Informatika (ITERA)</b> </h3>
     </div>
   </footer>
   </body>
