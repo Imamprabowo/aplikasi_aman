62fb331859beb5f1ee7581ac24b34955747a8e24
Frisky Riana Kesumadewi <38089317+friskyriana@users.noreply.github.com>
2019-07-29 11:06:09 +0700

    Update index.php

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,7 +1,6 @@
 <br><br><br>
     <div class="container text-center" style="margin: 2em auto;">
       <div class="jumbotron">
-        <h1 class="display-3">Welcome To</h1>
-      	<img src="<?php echo base_url('image/home2.png')?>">
+      	<img src="<?php echo base_url('image/AMANN.png')?>">
     </div>
   </div>
